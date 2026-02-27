0578f4ff27198cf554784fb78857980d1853c599
Md. Shibli Sadik <shiblisadik465@gmail.com>
2026-02-27 15:36:03 +0600

    my first commit

---
+++ index2.cpp
@@ -0,0 +1,27 @@
+// Bismillahir Rahmanir Raheem
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef double dl;
+typedef tree< ll, null_type, greater_equal<ll>, rb_tree_tag, tree_order_statistics_node_update > ordered_set;
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+#define u_b upper_bound
+#define l_b lower_bound
+#define setpre(n) fixed<<setprecision(n)
+#define MOD 1000000007
+const double eps = 1e-9;
+const double PI = acos(-1);
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cout << "I love Bangladesh\n";
+    return 0;
+}
